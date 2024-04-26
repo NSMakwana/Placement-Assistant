c7bb639cf0612d4b0ac949dd0a6abedf16d66793
maitribhalala <maitribhalala@gmail.com>
2024-04-26 21:37:12 +0530

    logins

--- enter_page/login.php
+++ enter_page/login.php
@@ -4,27 +4,47 @@
 
 if(isset($_POST["submit"])) 
 { 
-  //  $email_id=$_REQUEST["email_id"];
-    $query="SELECT * FROM  sign_up WHERE email_id=".$email_id;
-    $res=mysqli_query($conn,$query);
+    $email_id=$_POST["email_id"];
+    $password=$_POST["password"];
 
+    $query="select * from sign_up";
+    $flag=0;
+    $record=mysqli_query($conn,$query);
 
-    if(isset($email_id)) 
-    { 
-        if($email_id=$_POST["email_id"])
-            { 
-                $f=TRUE; 
-                if($f) 
-            { 
-            echo"successfully loggedin"; 
-            } 
-            else 
-            { 
-            echo "user not found"; 
-            } 
-            } 
+    while($result=mysqli_fetch_assoc($record))
+    {
+        if($result["email_id"]==$email_id && $result["password"]==$password)
+        {
+            $flag=1;
+        }
     }
-} 
+    if($flag==0)
+    {
+        echo"invalid data";
+        header("Location:login.php");
+    }
+    else
+    {
+        header("Loaction:home_page(blank).php");
+    }
+}
+
+//     if(isset($email_id)) 
+//     { 
+//         if($email_id=$_POST["email_id"])
+//             { 
+//                 $f=TRUE; 
+//                 if($f) 
+//             { 
+//             echo"successfully loggedin"; 
+//             } 
+//             else 
+//             { 
+//             echo "user not found"; 
+//             } 
+//             } 
+//     }
+// } 
  
 ?> 
 <html> 
