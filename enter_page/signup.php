feef70ba6db9f90e23fa84260206f166053a0369
maitribhalala <maitribhalala@gmail.com>
2024-04-26 16:01:08 +0530

    signup

---
+++ enter_page/signup.php
@@ -0,0 +1,38 @@
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <form method = "post">
+    NAME :
+    <br><input type = "text" name = "user_name"><br>
+
+    EMAIL ID:
+    <br><input type = "text" name = "email_id"><br>
+
+    PASSWORD:
+    <br><input type = "text" name = "password"><br>
+
+    <input type = "submit" name = "submit" value = "submit">
+    </form>
+
+<?php
+
+    require_once("connection.php");
+
+    if(isset($_POST["submit"]))
+    {
+        $user_name = $_POST["user_name"];
+        $email_id = $_POST["email_id"];
+        $password = $_POST["password"];
+
+        $query = "INSERT INTO sign_up(user_name,email_id,password)  VALUES('".$user_name."','".$email_id."','".$password."')";
+        mysqli_query($conn,$query);
+
+    }
+
+?>
+
+
+</body>
+</html>
